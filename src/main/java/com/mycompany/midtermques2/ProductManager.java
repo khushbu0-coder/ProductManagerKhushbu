49b274f2ceb4978d67a6edda85490d07d530e6f1
Khushbu Khushbu <khushbu0@sheridancollege.ca>
2026-02-18 14:12:20 -0500

    Initial commit by Khushbu

---
+++ src/main/java/com/mycompany/midtermques2/ProductManager.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.midtermques2;
+
+/**
+ *
+ * @author khush
+ */
+public class ProductManager {
+       public String greeting(){
+        return "Welcome to Product Manager Class";
+    }
+}
